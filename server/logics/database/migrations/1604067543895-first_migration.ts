7a3963012622da24ecd5ddd33f6820277e04edcd
Caleb Isaac Mora Diaz <calexico659@gmail.com>
2020-10-30 09:20:19 -0500

    Generacion de migraciones

--- server/logics/database/migrations/1604067543895-first_migration.ts
+++ server/logics/database/migrations/1604067543895-first_migration.ts
@@ -1,14 +1,16 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class migrationNameHere1604066966246 implements MigrationInterface {
-    name = 'migrationNameHere1604066966246'
+export class firstMigration1604067543895 implements MigrationInterface {
+    name = 'firstMigration1604067543895'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "session" ("sid" character varying COLLATE "default" NOT NULL, "sess" json NOT NULL, "expire" TIMESTAMP NOT NULL, CONSTRAINT "PK_7575923e18b495ed2307ae629ae" PRIMARY KEY ("sid"))`);
         await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`DROP TABLE "users"`);
+        await queryRunner.query(`DROP TABLE "session"`);
     }
 
 }
